3ba63e384b75913bd2abfff8939c5e1a87f65a9e
Florian Pigorsch <fpigorsch@gmail.com>
2024-02-25 19:17:16 +0100

    add scripts folder

---
+++ scripts/cronjob.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+TARGET="/var/www/virtual/floppnet/parkrun.flopp.net/"
+
+rm -rf "${SCRIPT_DIR}/.output"
+
+(cd "${SCRIPT_DIR}/repo" && go run cmd/generate/main.go \
+    -data     "${SCRIPT_DIR}/repo/data" \
+    -download "${SCRIPT_DIR}/.download" \
+    -output   "${SCRIPT_DIR}/.output")
+
+mkdir -p "${TARGET}"
+cp -a "${SCRIPT_DIR}/.output/." "${TARGET}"
+chmod -R a+rx "${TARGET}"
