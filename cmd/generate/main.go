62e3b299ed68e21577ac1100f329a00817719ad0
Florian Pigorsch <fpigorsch@gmail.com>
2024-03-01 17:36:26 +0100

    better description

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -21,13 +21,15 @@ type RenderData struct {
 	CssFiles       []string
 	StatsJs        string
 	Title          string
+	Description    string
 	Canonical      string
 	Nav            string
 	Timestamp      string
 }
 
-func (data *RenderData) set(title, canonical, nav string) {
+func (data *RenderData) set(title, description, canonical, nav string) {
 	data.Title = title
+	data.Description = description
 	data.Canonical = canonical
 	data.Nav = nav
 }
@@ -191,25 +193,25 @@ func main() {
 			archived += 1
 		}
 	}
-	renderData := RenderData{events, active, archived, js_files, css_files, statsJs, "", "", "", now.Format("2006-01-02 15:04:05")}
+	renderData := RenderData{events, active, archived, js_files, css_files, statsJs, "", "", "", "", now.Format("2006-01-02 15:04:05")}
 	t := PathBuilder(filepath.Join(*dataDir, "templates"))
-	renderData.set("parkruns in Deutschland - Karte", "https://parkrun.flopp.net/", "map")
+	renderData.set("parkrun Karte", "Karte aller deutschen parkruns mit Anzeige der einzelnen Laufstrecken und Informationen zum letzten Event", "https://parkrun.flopp.net/", "map")
 	if err := renderData.render(filepath.Join(*outputDir, "index.html"), t.Path("index.html"), t.Path("header.html"), t.Path("footer.html"), t.Path("tail.html")); err != nil {
 		panic(fmt.Errorf("while rendering 'index.html': %v", err))
 	}
-	renderData.set("parkruns in Deutschland - Liste", "https://parkrun.flopp.net/liste.html", "list")
+	renderData.set("parkrun Liste", "Liste aller deutschen parkruns mit Informationen zum letzten Event", "https://parkrun.flopp.net/liste.html", "list")
 	if err := renderData.render(filepath.Join(*outputDir, "liste.html"), t.Path("liste.html"), t.Path("header.html"), t.Path("footer.html"), t.Path("tail.html")); err != nil {
 		panic(fmt.Errorf("while rendering 'list.html': %v", err))
 	}
-	renderData.set("parkruns in Deutschland - Info", "https://parkrun.flopp.net/info.html", "info")
+	renderData.set("parkruns Karte - Info", "Informationen", "https://parkrun.flopp.net/info.html", "info")
 	if err := renderData.render(filepath.Join(*outputDir, "info.html"), t.Path("info.html"), t.Path("header.html"), t.Path("footer.html"), t.Path("tail.html")); err != nil {
 		panic(fmt.Errorf("while rendering 'info.html': %v", err))
 	}
-	renderData.set("parkruns in Deutschland - Datenschutz", "https://parkrun.flopp.net/datenschutz.html", "datenschutz")
+	renderData.set("parkruns Karte - Datenschutz", "Datenschutzinformationen", "https://parkrun.flopp.net/datenschutz.html", "datenschutz")
 	if err := renderData.render(filepath.Join(*outputDir, "datenschutz.html"), t.Path("datenschutz.html"), t.Path("header.html"), t.Path("footer.html"), t.Path("tail.html")); err != nil {
 		panic(fmt.Errorf("while rendering 'datenschutz.html': %v", err))
 	}
-	renderData.set("parkruns in Deutschland - Impressum", "https://parkrun.flopp.net/impressum.html", "impressum")
+	renderData.set("parkruns Karte - Impressum", "Impressum", "https://parkrun.flopp.net/impressum.html", "impressum")
 	if err := renderData.render(filepath.Join(*outputDir, "impressum.html"), t.Path("impressum.html"), t.Path("header.html"), t.Path("footer.html"), t.Path("tail.html")); err != nil {
 		panic(fmt.Errorf("while rendering 'impressum.html': %v", err))
 	}
